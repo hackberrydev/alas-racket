5004081767573b4370268a272341e6de0fb5f8b3
Nebojša Stričević <strika@hackberry.dev>
2021-06-21 05:35:59 +0000

    Implement function for formatting day title

--- main.rkt
+++ main.rkt
@@ -1,7 +1,21 @@
 #lang racket
 
+(require racket/date)
+
 (define (read-todo-file in)
   (display (port->string in)))
 
+(define (format-day-title date)
+  (string-append "## "
+                 (~a (date-year date))
+                 "-"
+                 (~a (date-month date) #:min-width 2 #:align 'right #:left-pad-string "0")
+                 "-"
+                 (~a (date-day date) #:min-width 2 #:align 'right #:left-pad-string "0")))
+
 (module* main #f
   (call-with-input-file "todo.md" read-todo-file))
+
+(module+ test
+  (require rackunit)
+  (check-equal? (format-day-title (current-date)) "## 2021-06-21"))
