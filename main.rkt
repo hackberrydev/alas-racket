089bfbc71952b78a72362780f974802a32276149
Nebojša Stričević <strika@hackberry.dev>
2021-06-22 04:56:09 +0000

    Use Gregor for dates

--- main.rkt
+++ main.rkt
@@ -1,21 +1,21 @@
 #lang racket
 
-(require racket/date)
+(require gregor)
 
 (define (read-todo-file in)
   (display (port->string in)))
 
 (define (format-day-title date)
   (string-append "## "
-                 (~a (date-year date))
+                 (~a (->year date))
                  "-"
-                 (~a (date-month date) #:min-width 2 #:align 'right #:left-pad-string "0")
+                 (~a (->month date) #:min-width 2 #:align 'right #:left-pad-string "0")
                  "-"
-                 (~a (date-day date) #:min-width 2 #:align 'right #:left-pad-string "0")))
+                 (~a (->day date) #:min-width 2 #:align 'right #:left-pad-string "0")))
 
 (module* main #f
   (call-with-input-file "todo.md" read-todo-file))
 
 (module+ test
   (require rackunit)
-  (check-equal? (format-day-title (current-date)) "## 2021-06-21"))
+  (check-equal? (format-day-title (date 2021 6 21)) "## 2021-06-21"))
