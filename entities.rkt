8a643eabf1cf89de27542c07b866e7a66701c387
Nebojša Stričević <strika@hackberry.dev>
2021-06-29 05:08:32 +0000

    Make entities mutable

--- entities.rkt
+++ entities.rkt
@@ -1,4 +1,4 @@
 #lang racket
 
-(struct day (date tasks line changed))
-(struct task (title body done line changed))
+(struct day (date tasks line changed) #:mutable)
+(struct task (title body done line changed) #:mutable)
