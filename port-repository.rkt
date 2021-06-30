33d94d6f92823116a1cc52e766521bb752f2b6a6
Nebojša Stričević <strika@hackberry.dev>
2021-06-30 05:10:09 +0000

    Add spec for load-todo

--- port-repository.rkt
+++ port-repository.rkt
@@ -6,6 +6,9 @@
 (define (day-title day)
   (string-append "## " (~t (day-date day) "y-MM-dd, EEEE")))
 
+(define (load-todo port)
+  (list))
+
 (module+ test
   (require rackunit)
 
@@ -13,4 +16,17 @@
     "day-title"
     (check-equal?
       (day-title (day (date 2021 6 21) '() 1 false))
-      "## 2021-06-21, Monday")))
+      "## 2021-06-21, Monday"))
+
+  (test-case
+    "load-todo"
+    (let* ([todo (string-append "# Main TODO\n\n"
+                                "## 2020-08-01, Saturday\n\n"
+                                "- [ ] Develop photos\n"
+                                "- [x] Pay bills\n\n"
+                                "## 2020-07-31, Friday\n\n"
+                                "- [x] Review open pull requests\n"
+                                "- [x] Fix the flaky test")]
+           [port (open-input-string todo)]
+           [days (load-todo port)])
+      (check-equal? (length days) 2))))
