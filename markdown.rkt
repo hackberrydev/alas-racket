dff4374048b6de07b01d7cb81a469a97e8dbe464
Nebojša Stričević <strika@hackberry.dev>
2021-07-12 05:24:47 +0000

    Move load-todo to markdown module

--- markdown.rkt
+++ markdown.rkt
@@ -6,11 +6,54 @@
 (define (day-title day)
   (string-append "## " (~t (day-date day) "y-MM-dd, EEEE")))
 
+(define (day-title? line) (string-prefix? line "## "))
+
+(define (build-day line line-number)
+  (day (iso8601->date (substring line 3 13))
+       (list)
+       line-number
+       #f))
+
+(define (parse todo)
+  (define (parse-line todo-lines days line-number)
+    (if (empty? todo-lines)
+      (reverse days)
+      (let ([line (first todo-lines)]
+            [todo-lines (rest todo-lines)]
+            [line-number (+ line-number 1)])
+        (cond
+          [(day-title? line) (parse-line
+                               todo-lines
+                               (cons (build-day line line-number) days)
+                               line-number)]
+          [else (parse-line todo-lines days line-number)]))))
+  (parse-line (string-split todo "\n") (list) 0))
+
 (module+ test
   (require rackunit)
 
   (test-case
     "day-title"
     (check-equal?
-      (day-title (day (date 2021 6 21) '() 1 0 false))
-      "## 2021-06-21, Monday")))
+      (day-title (day (date 2021 6 21) '() 1 false))
+      "## 2021-06-21, Monday"))
+
+  (test-case
+    "parse"
+    (let* ([todo (string-append "# Main TODO\n\n"
+                                "## 2020-08-01, Saturday\n\n"
+                                "- [ ] Develop photos\n"
+                                "- [x] Pay bills\n\n"
+                                "## 2020-07-31, Friday\n\n"
+                                "- [x] Review open pull requests\n"
+                                "- [x] Fix the flaky test")]
+           [days (parse todo)]
+           [day-1 (list-ref days 0)]
+           [day-2 (list-ref days 1)])
+      (check-equal? (length days) 2)
+      (check-equal? (day-date day-1) (date 2020 8 1))
+      (check-equal? (day-line-number day-1) 3)
+      (check-false (day-changed day-1))
+      (check-equal? (day-date day-2) (date 2020 7 31))
+      (check-equal? (day-line-number day-2) 8)
+      (check-false (day-changed day-2)))))
--- port-repository.rkt
+++ port-repository.rkt
@@ -3,30 +3,6 @@
 (require gregor
          "entities.rkt")
 
-(define (build-day line line-number column position)
-  (day (iso8601->date (substring line 3 13))
-       (list)
-       line-number
-       position
-       #f))
-
-(define (day-line? line) (string-prefix? line "## "))
-
-(define (load-todo port)
-  (define (load-line port days)
-    (let*-values ([(line-number column position) (port-next-location port)]
-                  [(line) (read-line port)])
-      (cond
-        [(eof-object? line) (reverse days)]
-        [(day-line? line) (load-line port (cons (build-day line
-                                                           line-number
-                                                           column
-                                                           position)
-                                                days))]
-        [else (load-line port days)])))
-  (port-count-lines! port)
-  (load-line port (list)))
-
 (define (save-day day port)
   (set-port-next-location! port (day-line-number day) 0 (day-position day))
   (display (day-title day) port)
@@ -39,27 +15,6 @@
 
 (module+ test
   (require rackunit)
-
-  (test-case
-    "load-todo"
-    (let* ([todo (string-append "# Main TODO\n\n"
-                                "## 2020-08-01, Saturday\n\n"
-                                "- [ ] Develop photos\n"
-                                "- [x] Pay bills\n\n"
-                                "## 2020-07-31, Friday\n\n"
-                                "- [x] Review open pull requests\n"
-                                "- [x] Fix the flaky test")]
-           [port (open-input-string todo)]
-           [days (load-todo port)]
-           [day-1 (list-ref days 0)]
-           [day-2 (list-ref days 1)])
-      (check-equal? (length days) 2)
-      (check-equal? (day-date day-1) (date 2020 8 1))
-      (check-equal? (day-line-number day-1) 3)
-      (check-false (day-changed day-1))
-      (check-equal? (day-date day-2) (date 2020 7 31))
-      (check-equal? (day-line-number day-2) 8)
-      (check-false (day-changed day-2))))
 
   (test-case
     "save-todo"
