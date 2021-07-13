e5535499ff56393bdff3aa80e0e57418795d1d24
Nebojša Stričević <strika@hackberry.dev>
2021-07-13 05:29:34 +0000

    Implement serializing to string

---
+++ string-repository.rkt
@@ -0,0 +1,100 @@
+#lang racket
+
+(require gregor
+         "entities.rkt")
+
+(define (day-title day)
+  (string-append "## " (~t (day-date day) "y-MM-dd, EEEE") "\n"))
+
+(define (day-title? line) (string-prefix? line "## "))
+
+(define (build-day line line-number)
+  (day (iso8601->date (substring line 3 13))
+       (list)
+       line-number
+       #f))
+
+(define (parse todo)
+  (define (parse-line todo-lines days line-number)
+    (if (empty? todo-lines)
+      (reverse days)
+      (let ([line (first todo-lines)]
+            [todo-lines (rest todo-lines)]
+            [line-number (+ line-number 1)])
+        (cond
+          [(day-title? line) (parse-line
+                               todo-lines
+                               (cons (build-day line line-number) days)
+                               line-number)]
+          [else (parse-line todo-lines days line-number)]))))
+  (parse-line (string-split todo "\n") (list) 0))
+
+(define (serialize days todo)
+  (define (do-something days todo-lines new-todo-lines current-line)
+    (if (empty? todo-lines)
+      (string-join (reverse new-todo-lines) "\n")
+      (let ([current-line (+ current-line 1)]
+            [day (first days)]
+            [line (first todo-lines)]
+            [todo-lines (rest todo-lines)])
+        (if (= (day-line-number day) current-line)
+          (do-something (rest days)
+                        todo-lines
+                        (cons line (cons (day-title day) new-todo-lines))
+                        current-line)
+          (do-something days
+                        todo-lines
+                        (cons line new-todo-lines)
+                        current-line)))))
+  (do-something days (string-split todo "\n") (list) 0))
+
+(module+ test
+  (require rackunit)
+
+  (test-case
+    "day-title"
+    (check-equal?
+      (day-title (day (date 2021 6 21) '() 1 false))
+      "## 2021-06-21, Monday\n"))
+
+  (test-case
+    "parse"
+    (let* ([todo (string-append "# Main TODO\n\n"
+                                "## 2020-08-01, Saturday\n\n"
+                                "- [ ] Develop photos\n"
+                                "- [x] Pay bills\n\n"
+                                "## 2020-07-31, Friday\n\n"
+                                "- [x] Review open pull requests\n"
+                                "- [x] Fix the flaky test")]
+           [days (parse todo)]
+           [day-1 (list-ref days 0)]
+           [day-2 (list-ref days 1)])
+      (check-equal? (length days) 2)
+      (check-equal? (day-date day-1) (date 2020 8 1))
+      (check-equal? (day-line-number day-1) 3)
+      (check-false (day-changed day-1))
+      (check-equal? (day-date day-2) (date 2020 7 31))
+      (check-equal? (day-line-number day-2) 8)
+      (check-false (day-changed day-2))))
+
+  (test-case
+    "serialize"
+    (let* ([todo (string-append "# Main TODO\n\n"
+                                "## 2020-08-01, Saturday\n\n"
+                                "- [ ] Develop photos\n"
+                                "- [x] Pay bills\n\n"
+                                "## 2020-07-31, Friday\n\n"
+                                "- [x] Review open pull requests\n"
+                                "- [x] Fix the flaky test")]
+           [days (list (day (date 2020 8 2) (list) 3 #t)
+                       (day (date 2020 8 1) (list) 3 #f)
+                       (day (date 2020 7 31) (list) 8 #f))])
+      (check-equal? (serialize days todo)
+                    (string-append "# Main TODO\n\n"
+                                   "## 2020-08-02, Sunday\n\n"
+                                   "## 2020-08-01, Saturday\n\n"
+                                   "- [ ] Develop photos\n"
+                                   "- [x] Pay bills\n\n"
+                                   "## 2020-07-31, Friday\n\n"
+                                   "- [x] Review open pull requests\n"
+                                   "- [x] Fix the flaky test")))))
