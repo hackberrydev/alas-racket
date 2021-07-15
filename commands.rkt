171ad03b81e8a2e5a234e8f5b2fb0b3bbb7bf4d6
Nebojša Stričević <strika@hackberry.dev>
2021-07-15 05:24:46 +0000

    Add skeleton for insert-days

---
+++ commands.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(require gregor
+         "entities.rkt")
+
+(define (insert-days days date)
+  days)
+
+(module+ test
+  (require rackunit)
+
+  (test-case
+    "insert-days"
+    (let* ([days (list (day (date 2020 8 3) (list) 3 #f)
+                       (day (date 2020 8 2) (list) 6 #f))]
+           [new-days (insert-days days (date 2020 8 4))]
+           [day-1 (first new-days)])
+      (check-equal? (length new-days) 3)
+      (check-equal? (day-date day-1) (date 2020 8 4))
+      (check-equal? (day-line-number day-1) 3)
+      (check-true (day-changed day-1)))))
