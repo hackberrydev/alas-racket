5e7d91456fa4897838ee8934c1adc126d577f58b
Nebojša Stričević <strika@hackberry.dev>
2021-08-11 05:12:41 +0000

    Make tar archive in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+echo "raco exe..."
 raco exe main.rkt
+
+echo "raco distribute..."
 raco distribute distribution main
+
+echo "tar..."
+tar cvf alas.tar distribution/
+
+echo "done."
