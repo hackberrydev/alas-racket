b63d07299f8fc9c915c537dfccc79088ff66feae
Nebojša Stričević <strika@hackberry.dev>
2021-08-11 05:28:47 +0000

    Update insert-days to support day in middle

--- commands.rkt
+++ commands.rkt
@@ -24,9 +24,14 @@
 (define (insert-days days date)
   (if (empty? days)
     (generate-days (-days date 1) date 1)
-    (let ([day (first days)])
-      (append (generate-days (day-date day) date (day-line-number day))
-              days))))
+    (let*-values ([(days-before days-after) (splitf-at
+                                              days
+                                              (lambda (d)
+                                                (date>? (day-date d) date)))]
+                  [(day) (first days-after)])
+      (append days-before
+              (generate-days (day-date day) date (day-line-number day))
+              days-after))))
 
 ; Public: Runs commands on a todo.
 ;
@@ -67,6 +72,17 @@
       (check-equal? (day-date day-1) (date 2020 8 4))
       (check-equal? (day-line-number day-1) 3)
       (check-true (day-changed day-1))))
+
+  (test-case
+    "insert-days with a day in the middle"
+    (let* ([days (list (day (date 2020 8 5) (list) 3 #f)
+                       (day (date 2020 8 2) (list) 6 #f))]
+           [new-days (insert-days days (date 2020 8 4))]
+           [day-2 (list-ref new-days 1)])
+      (check-equal? (length new-days) 4)
+      (check-equal? (day-date day-2) (date 2020 8 4))
+      (check-equal? (day-line-number day-2) 6)
+      (check-true (day-changed day-2))))
 
   (test-case
     "insert-days with empty days list"
