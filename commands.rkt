ff079ea0f0d96a4e06b3a88491d83aef1b9e3c4a
Nebojša Stričević <strika@hackberry.dev>
2021-08-18 05:31:07 +0000

    Handle case with one day in future

--- commands.rkt
+++ commands.rkt
@@ -10,14 +10,31 @@
 (define (insert-day days date)
   (define (day-after-date? day)
     (date>? (day-date day) date))
-  (let*-values ([(days-after days-before) (splitf-at days day-after-date?)])
-    (if (empty? days-before)
-      (list (day date (list) 1 #t))
-      (if (date=? (day-date (first days-before)) date)
+  (let-values ([(days-after days-before) (splitf-at days day-after-date?)])
+    (let ([adjacent-day (if (empty? days-before)
+                          (last days-after)
+                          (first days-before))])
+      (if (date=? (day-date adjacent-day) date)
         days
         (append days-after
-                (list (day date (list) (day-line-number (first days-before)) #t))
+                (list (day date (list) (day-line-number adjacent-day) #t))
                 days-before)))))
+
+; Private: Insert new days into the list of day entities. The list must be not
+;          empty.
+;
+; days  - The list of day entities.
+; date  - The date up to which new days will be generated.
+; today - Date.
+;
+; Returns a new list of days.
+(define (insert-days-in-list days date today)
+  (if (date<? date today)
+    days
+    (insert-days (insert-day days date) (-days date 1) today)))
+
+(define (build-first-day date)
+  (list (day date (list) 1 #t)))
 
 ; Public: Insert new days into the list of day entities.
 ;
@@ -27,9 +44,9 @@
 ;
 ; Returns a new list of days.
 (define (insert-days days date today)
-  (if (date<? date today)
-    days
-    (insert-days (insert-day days date) (-days date 1) today)))
+  (if (empty? days)
+    (build-first-day date)
+    (insert-days-in-list days date today)))
 
 ; Public: Runs commands on a todo.
 ;
@@ -99,6 +116,15 @@
     (let ([days (insert-days (list) (date 2020 8 4) (date 2020 8 4))])
       (check-equal? (length days) 1)
       (check-equal? (day-date (first days)) (date 2020 8 4))))
+
+  (test-case
+    "insert-days with one day in future"
+    (let* ([days (list (day (date 2020 8 5) (list) 3 #f))]
+           [new-days (insert-days days (date 2020 8 4) (date 2020 8 4))]
+           [day-2 (second new-days)])
+      (check-equal? (length new-days) 2)
+      (check-equal? (day-date day-2) (date 2020 8 4))
+      (check-equal? (day-line-number day-2) 3)))
 
   (test-case
     "run-commands"
