7507a72773c2b1767cc90e1ddfcc9f3d314d40d5
Nebojša Stričević <strika@hackberry.dev>
2021-08-20 05:38:12 +0000

    Use raco-cross to build package for MacOS

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+}
+
+echo "install dependencies.."
+raco cross --target aarch64-macosx pkg install gregor
+
 echo "raco exe..."
-raco exe main.rkt
+raco cross --target aarch64-macosx exe main.rkt
 
 echo "raco distribute..."
-raco distribute distribution main
+raco cross --target aarch64-macosx dist distribution main
 
 echo "tar..."
 tar cvf alas.tar distribution/
--- info.rkt
+++ info.rkt
@@ -3,4 +3,4 @@
 (define collection "alas")
 (define version "0.0.1")
 (define deps '("gregor"))
-(define build-deps '("rackunit-lib"))
+(define build-deps '("rackunit-lib" "raco-cross"))
