f415325b534a1dddab66df020070791580204f0e
Nebojša Stričević <strika@hackberry.dev>
2021-08-31 05:49:32 +0000

    Use raco cross to build package for Windows

--- build.sh
+++ build.sh
@@ -7,16 +7,27 @@ cleanup() {
   trap - SIGINT SIGTERM ERR EXIT
 }
 
-echo "install dependencies.."
-raco cross --target aarch64-macosx pkg install gregor
+build() {
+  echo "building for $1..."
+  echo "install dependencies.."
+  raco cross --target $1 pkg install --skip-installed --auto gregor
 
-echo "raco exe..."
-raco cross --target aarch64-macosx exe main.rkt
+  echo "raco exe..."
+  raco cross --target $1 exe --embed-dlls main.rkt
 
-echo "raco distribute..."
-raco cross --target aarch64-macosx dist distribution main
+  echo "raco distribute..."
+  if [ $1 = "x86_64-win32" ]; then
+    raco cross --target $1 distribute distribution-$1 main.exe
+  else
+    raco cross --target $1 distribute distribution-$1 main
+  fi
 
-echo "tar..."
-tar cvf alas.tar distribution/
+  echo "tar..."
+  tar cvf alas-$1.tar distribution-$1/
 
-echo "done."
+  echo "done for $1"
+}
+
+build x86_64-linux
+build aarch64-macosx
+build x86_64-win32
